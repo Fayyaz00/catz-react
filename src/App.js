6f80f31c411f94a9e9573d1ced86d2a9f5625fb1
Fayyaz00 <fayyazsulman@gmail.com>
2021-04-14 01:05:25 -0400

    Basics of App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const App = () => {
 
-export default App;
+  console.log('Hello from component')
+
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <div>
+        <h1>Greetings</h1>
+        <Hello />
+      </div>
+    </div>
+  )
+}
+
+
+export default App
