6500ab64d7af5276ef6e43ca2450f9f9ededf98a
Fayyaz00 <fayyazsulman@gmail.com>
2021-04-17 22:57:42 -0400

    2d: Sending Data to the Server

--- src/App.js
+++ src/App.js
@@ -22,16 +22,20 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important === true);
 
-  const addNote = (event) => {
+  const addNote = event => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+      date: new Date(),
+      important: Math.random() > 0.5,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
